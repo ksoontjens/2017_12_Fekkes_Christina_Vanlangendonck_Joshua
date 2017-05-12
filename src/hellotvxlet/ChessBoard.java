d93faf0edca0eeff6fbb75ec83631b62a08bb49c
JoshuaVanlangendonck <joshua@winak.be>
2017-05-12 10:37:51 +0200

    added damstukjes

--- src/hellotvxlet/ChessBoard.java
+++ src/hellotvxlet/ChessBoard.java
@@ -5,6 +5,7 @@
 
 package hellotvxlet;
 
+//import com.sun.net.ssl.internal.ssl.Debug;
 import java.awt.Color;
 import java.awt.Graphics;
 import org.bluray.ui.event.HRcEvent;
@@ -24,9 +25,21 @@ public class ChessBoard extends HComponent implements UserEventListener {
     int curx=0; //welk vakje x je bent
     int cury=0; //vakje y
     
+    int[][] bordArray = new int[8][8];
+    
     public ChessBoard()
     {
         this.setBounds(0,0,720,576); // full screen
+        for(int i = 0; i<bordArray.length; i++) {
+            for(int j = 0; j<bordArray.length; j++) {
+            if(i<4 || j>6) {
+                bordArray[i][j] = 1;
+            }
+            else{
+                bordArray[i][j] = 0;
+            }
+        }
+        }
     }
     
     public void paint(Graphics g)
@@ -35,16 +48,43 @@ public class ChessBoard extends HComponent implements UserEventListener {
         {
             for (int y=0;y<8;y++)
             {
-                if ((x+y)%2==0) g.setColor(Color.WHITE); 
-                else g.setColor(Color.BLACK);
+                if ((x+y)%2==0) {
+                    g.setColor(Color.BLACK);
+                    g.fillRect(x*50+xoff, y*50+yoff, 50, 50);
+                    if(bordArray[x][y] == 1) {
+                        g.setColor(Color.BLUE);
+                        g.fillOval(x*50+xoff, y*50+xoff, 40, 40);
+                        //g.setColor(Color.WHITE);
+                    }
+                    else {
+                        
+                    }
+                    g.setColor(Color.WHITE);
+                    g.fillRect(x*50+xoff, y*50+yoff, 50, 50);
+                    
+                }
+                else {
+                    g.setColor(Color.BLACK);
+                    g.fillRect(x*50+xoff, y*50+yoff, 50, 50);
+                    if(bordArray[x][y] == 1) {
+                        g.setColor(Color.BLUE);
+                        g.fillOval(x*50+xoff, y*50+xoff, 40, 40);
+                        //g.setColor(Color.WHITE);
+                    }
+                    else {
+                        
+                    }
+                    
+                }
                 
-                g.fillRect(x*50+xoff, y*50+yoff, 50, 50);
-
+                
+                
+                
             }
                 
         }
         g.setColor(Color.BLUE);
-        g.drawRect(curx*50+xoff, cury*50+yoff, 50, 50);
+        g.drawRect(curx*50+xoff, cury*50+yoff, 50, 50); //teken selectievakje
     }
 
     public void userEventReceived(UserEvent e) {
